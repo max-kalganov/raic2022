bf09f2d41946a1f721b92873e224df8d7971bc1f
max <maksim.kalganov@gmail.com>
2022-07-16 17:41:29 +0300

    slightly update strategy

--- my_strategy.py
+++ my_strategy.py
@@ -1,10 +1,11 @@
+from model import Unit
 from model.game import Game
 from model.order import Order
 from model.unit_order import UnitOrder
 from model.constants import Constants
 from model.vec2 import Vec2
 from model.action_order import ActionOrder
-from typing import Optional
+from typing import Optional, Tuple, List
 from debug_interface import DebugInterface
 
 
@@ -12,16 +13,39 @@ class MyStrategy:
     def __init__(self, constants: Constants):
         pass
 
-    def get_order(self, game: Game, debug_interface: Optional[DebugInterface]) -> Order:
-        orders = {}
+    def process_units(self, game: Game) -> Tuple[Unit, List[Unit]]:
+        me, opponents = None, []
         for unit in game.units:
             if unit.player_id != game.my_id:
-                continue
-            orders[unit.id] = UnitOrder(
-                Vec2(-unit.position.x, -unit.position.y),
-                Vec2(-unit.direction.y, unit.direction.x),
-                ActionOrder.Aim(True))
-        return Order(orders)
+                opponents.append(unit)
+            else:
+                me = unit
+        assert me is not None
+        return me, opponents
+
+    def get_default_order(self, my_unit: Unit):
+        return UnitOrder(
+            target_velocity=Vec2(-my_unit.position.x, -my_unit.position.y),
+            target_direction=Vec2(-my_unit.direction.y, my_unit.direction.x),
+            action=ActionOrder.Aim(False)
+        )
+
+    def see_opponents_order(self, my_unit: Unit, opponents: List[Unit]):
+        return UnitOrder(
+            target_velocity=Vec2(-my_unit.position.x, -my_unit.position.y),
+            target_direction=Vec2(-my_unit.direction.y, my_unit.direction.x),
+            action=ActionOrder.Aim(True)
+        )
+
+    def get_order(self, game: Game, debug_interface: Optional[DebugInterface]) -> Order:
+        my_unit, opponents = self.process_units(game)
+
+        if opponents:
+            my_order = self.see_opponents_order(my_unit, opponents)
+        else:
+            my_order = self.get_default_order(my_unit)
+
+        return Order({my_unit.id: my_order})
 
     def debug_update(self, displayed_tick: int, debug_interface: DebugInterface):
         pass
